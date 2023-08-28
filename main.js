e96e136ba54ebc5b3e9ae24c1ff5ccd027356b80
Nik Uzair <nikuzairsc@gmail.com>
2023-08-28 11:03:31 +0800

    Add gradient-background cursor

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-import './app/scss/style.scss';
+console.log('working');
+
+const blob = document.getElementById('blob');
+
+document.body.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    { left: `${clientX}px`, top: `${clientY}px` },
+    { duration: 3000, fill: 'forwards' }
+  );
+};

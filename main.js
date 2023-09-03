ef260058a5c0221ba7d593983adb53d0c84ddb7d
Nik Uzair <nikuzairsc@gmail.com>
2023-09-03 20:26:29 +0800

    Add resume transition and style

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ document.body.onpointermove = (event) => {
     { duration: 2500, fill: 'forwards' }
   );
 };
+
+document.getElementById('projects').onmousemove = (e) => {
+  for (const card of document.getElementsByClassName('card')) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty('--mouse-x', `${x}px`);
+    card.style.setProperty('--mouse-y', `${y}px`);
+  }
+};

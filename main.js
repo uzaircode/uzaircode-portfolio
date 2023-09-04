1251ebe4070024d9c1cb041614a0d337514f2239
Nik Uzair <nikuzairsc@gmail.com>
2023-09-04 23:50:24 +0800

    Rename from style to main
    - WIP - move code to their respective folder

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-import "./app/scss/style.scss";
+import './app/scss/main.scss';
 
-console.log("working");
+console.log('working');
 
-const blob = document.getElementById("blob");
+const blob = document.getElementById('blob');
 
 document.body.onpointermove = (event) => {
   const { pageX, pageY } = event;
 
   blob.animate(
     { left: `${pageX}px`, top: `${pageY}px` },
-    { duration: 2500, fill: "forwards" },
+    { duration: 2500, fill: 'forwards' }
   );
 };
 
-document.getElementById("projects").onmousemove = (e) => {
-  for (const card of document.getElementsByClassName("card")) {
+document.getElementById('projects').onmousemove = (e) => {
+  for (const card of document.getElementsByClassName('card')) {
     const rect = card.getBoundingClientRect(),
       x = e.clientX - rect.left,
       y = e.clientY - rect.top;
 
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
+    card.style.setProperty('--mouse-x', `${x}px`);
+    card.style.setProperty('--mouse-y', `${y}px`);
   }
 };

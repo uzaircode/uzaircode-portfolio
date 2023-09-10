dcccf53e0666dfd966db3e59ef657f56a603621d
Nik Uzair <nikuzairsc@gmail.com>
2023-09-10 11:55:08 +0800

    Configure vite
    - Fix pdf link not working
    - Add additional HTML

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: "/uzaircode-portfolio/",
-  // ...
+  base: '/uzaircode-portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Your main entry point
+        project: resolve(__dirname, 'archive_project.html'), // Your additional entry point
+        resume: resolve(__dirname, 'public/Nik_Uzair_Resume.pdf'),
+      },
+    },
+  },
 });
